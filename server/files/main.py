4085b1de122afca433aebd570deac7f5e67e93b2
Paul <paulbelches@gmail.com>
2021-09-03 08:06:34 -0600

    .

--- server/files/main.py
+++ server/files/main.py
@@ -1,5 +1,7 @@
+
 from flask import Flask, request
 from flask_cors import CORS
+import subprocess
 
 app = Flask(__name__)
 CORS(app)
@@ -7,8 +9,12 @@ CORS(app)
 @app.route('/', methods=['POST'])
 def hello_world():
     data = request.json['code']
-    
-    return data
+    command = ["../../compiler/main", data]
+    result = subprocess.run(command, capture_output=True, encoding='ascii')
+    if (result.stderr == ''):
+        return result.stdout
+    else:
+        return result.stderr
 
 if __name__ == '__main__':
     app.run()
